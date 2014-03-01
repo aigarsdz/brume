ff16926bdf7d8af55023c2791fd70dec9e78c71b
Aigars Dzerviniks <dzerviniks.aigars@outlook.com>
2014-03-01 12:54:56 +0200

    Remove JSHint from scripts task

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,7 @@ var gulp    = require('gulp'),
     coffee  = require('gulp-coffee'),
     sass    = require('gulp-sass'),
     minify  = require('gulp-minify-css'),
-    prefix  = require('gulp-autoprefixer'),
-    jshint  = require('gulp-jshint');
+    prefix  = require('gulp-autoprefixer');
 
 var paths = {
   scripts: ['public/js/*.coffee'],
@@ -16,9 +15,6 @@ var paths = {
 gulp.task('scripts', function () {
   gulp.src(paths.scripts)
       .pipe(coffee({ bare: true }).on('error', gutil.log))
-      .pipe(jshint())
-      .pipe(jshint.reporter('default'))
-      .pipe(jshint.reporter('fail'))
       .pipe(uglify())
       .pipe(concat('main.js'))
       .pipe(gulp.dest('./public/js'));
